d6f58a49463065b3735b7960e73a109fe542b104
Ajinkya Wagh <ajinkyayt@gmail.com>
2022-04-21 16:59:53 +0530

    Customers added func create, edit, delete

--- controllers/Customers.js
+++ controllers/Customers.js
@@ -1,6 +1,41 @@
 const db = require('../util/database');
 
 
+exports.createCustomer = (req, res, next) => {
+    const custName = req.body.custName;
+    const custEmail = req.body.custEmail;
+    const custNumber = req.body.custNumber;
+    const custPoints = req.body.custPoints;
+
+    db.query("insert into customers (custname, custemail, custnum, custpoints) values ('" + custName + "', '" + custEmail + "', " + custNumber + " , " + custPoints + ")", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+exports.editCustomer = (req, res, next) => {
+    const custName = req.body.custName;
+    const custId = req.body.custId;
+    const custEmail = req.body.custEmail;
+    const custNumber = req.body.custNumber;
+    const custPoints = req.body.custPoints;
+
+    db.query("update customers set custname='"+ custName +"', custemail='"+ custEmail +"', custnum="+ custNumber +", custpoints="+ custPoints +" where custid='"+ custId +"'", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+exports.deleteCustomer = (req, res, next) => {
+    const custId = req.body.custId;
+
+    db.query("delete from customers where custid='"+ custId +"'", (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+
 exports.getALLCustomers = async (req, res, next) => {
         db.query("select * from customers", function (err, result) {
             if (err) throw err;
--- routes/Customers.js
+++ routes/Customers.js
@@ -6,4 +6,10 @@ const router = express.Router();
 
 router.get('/customers', customerController.getALLCustomers);
 
+router.post('/customers/add_customer', customerController.createCustomer);
+
+router.post('/customers/edit_customer', customerController.editCustomer);
+
+router.post('/customers/remove_customer', customerController.deleteCustomer);
+
 module.exports = router;
